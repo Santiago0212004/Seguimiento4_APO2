389dcf92c08556c98d8ecdb72a15c552c90f71f8
Santiago0212 <sjbs0212@gmail.com>
2022-02-21 08:42:16 -0500

    21-02-22

---
+++ src/control/ChangePasswordControl.java
@@ -0,0 +1,27 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+
+public class ChangePasswordControl {
+
+    @FXML
+    private Button changePassBTN;
+
+    @FXML
+    private Button closeBTN;
+
+    @FXML
+    private TextArea contentTA;
+
+    @FXML
+    void changePass(ActionEvent event) {
+
+    }
+
+    @FXML
+    void close(ActionEvent event) {
+
+    }
+
+}
